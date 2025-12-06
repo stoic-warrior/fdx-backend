1e200c623647b8e38c5583d61c449c70df096add
kjh <kbskjh2@gmail.com>
2025-12-06 18:32:33 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/fdx/backend/FdxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fdx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdx/backend/FdxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdx.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
