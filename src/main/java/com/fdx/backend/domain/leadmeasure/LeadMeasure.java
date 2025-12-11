4064140ee83c1c96774a8223b5fd67277e29cf97
kjh <kbskjh2@gmail.com>
2025-12-11 19:16:04 +0900

    feat: WIG CRUD API 구현 완료

---
+++ src/main/java/com/fdx/backend/domain/leadmeasure/LeadMeasure.java
@@ -0,0 +1,14 @@
+package com.fdx.backend.domain.leadmeasure;
+
+/**
+ * Lead Measure (선행지표) 엔티티
+ * 4DX: 목표(Lag Measure) 달성을 위한 핵심 활동 지표
+ *
+ * 예시:
+ * - 목표가 "체중 감량"이면, Lead Measure는 "운동 시간", "칼로리 섭취"
+ * - 목표가 "취업"이면, Lead Measure는 "코딩 시간", "이력서 제출"
+ */
+public class LeadMeasure {
+
+
+}
