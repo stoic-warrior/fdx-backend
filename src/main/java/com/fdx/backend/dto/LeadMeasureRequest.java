969ce0770b1ac260caf3cf42f05894b1c2f609ea
kjh <kbskjh2@gmail.com>
2025-12-15 20:16:02 +0900

    feat: LeadMeasureRequest

--- src/main/java/com/fdx/backend/dto/LeadMeasureRequest.java
+++ src/main/java/com/fdx/backend/dto/LeadMeasureRequest.java
@@ -1,4 +1,36 @@
 package com.fdx.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Lead Measure 생성/수정 요청 DTO
+ */
+@Data // @Getter @Setter
+@NoArgsConstructor // JSON 역직렬화에 필요. HTTP로 전달받은 json을 자바 인스턴스의 필드로 주입. 잭슨이 리플렉션을 활용
+@AllArgsConstructor // 테스트용
+@Builder
 public class LeadMeasureRequest {
+
+    @NotBlank(message = "선행지표 이름은 필수입니다")
+    private String name;
+
+    @NotNull(message = "일일 목표는 필수입니다")
+    @Positive(message = "일일 목표는 0보다 커야 합니다")
+    private Double dailyTarget;
+
+    @NotNull(message = "주간 목표는 필수입니다")
+    @Positive(message = "주간 목표는 0보다 커야 합니다")
+    private Double weeklyTarget;
+
+    @NotBlank(message = "단위는 필수입니다")
+    private String unit;
+
+    @NotNull(message = "WIG ID는 필수입니다")
+    private Long wigId;
 }
