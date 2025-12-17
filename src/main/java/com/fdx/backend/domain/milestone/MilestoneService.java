ea47e05473b5081d31e7c3d0198a171150fa9528
kjh <kbskjh2@gmail.com>
2025-12-18 01:56:09 +0900

    WIP: 마일스톤서비스

--- src/main/java/com/fdx/backend/domain/milestone/MilestoneService.java
+++ src/main/java/com/fdx/backend/domain/milestone/MilestoneService.java
@@ -1,4 +1,24 @@
 package com.fdx.backend.domain.milestone;
 
+import com.fdx.backend.domain.wig.WigRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Milestone 비즈니스 로직 처리 Service
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
 public class MilestoneService {
+
+    private final MilestoneRepository milestoneRepository;
+    private final WigRepository wigRepository;
+
+    /**
+     * 특정 WIG의 모든 Milestones 조회
+     */
 }
